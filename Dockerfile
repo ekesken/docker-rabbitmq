25956f02ad6bc632dd4437a5c5fcf0a845585b9c
Erhan KESKEN <erhan.kesken@ebrandvalue.com>
2015-12-16 10:23:51 +0200

    cleaned code and fixed problems, made it work with different ports.
    * made it work with different ports.
    * made it work with initially empty rabbitmq folders, due to #2.
    * added MARATHON_URI environment variable, due to #3.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 FROM rabbitmq:3.5-management
 
+ENV RABBITMQ_ERLANG_COOKIE= \
+  RABBITMQ_DEFAULT_USER=guest \
+  RABBITMQ_DEFAULT_PASS=guest \
+  RABBITMQ_DEFAULT_VHOST=/ \
+  RABBITMQ_NODE_PORT=5672 \
+  RABBITMQ_DIST_PORT=25672 \
+  ERL_EPMD_PORT=4369 \
+  RABBITMQ_MANAGEMENT_PORT=15672
+
 RUN apt-get update && apt-get install -y python python-pip
 
+RUN chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
 ADD ./rabbitmq-cluster.py /rabbitmq-cluster.py
 RUN chmod +x /rabbitmq-cluster.py
 
-ADD ./rabbitmq.config /etc/rabbitmq/rabbitmq.config
-
-RUN chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
-
 ENTRYPOINT ["/rabbitmq-cluster.py"]
-
--- rabbitmq-cluster.py
+++ rabbitmq-cluster.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import logging
 import os
 import subprocess
 import time
@@ -7,61 +7,146 @@ import urllib3
 import json
 
 
+LOGGER = logging.getLogger(__name__)
 APP_ID = os.getenv('MARATHON_APP_ID')
+MARATHON_URI = os.environ.get('MARATHON_URI', 'http://leader.mesos:8080')
+HOST_IP = os.getenv('HOST', '127.0.0.1')
+HOST_NAME = os.getenv('HOSTNAME')  # docker container_id
 
-# add extra entry for hosts
-host = os.getenv('HOST')
-if not host:
-    host = '127.0.0.1'
-
-current_host = host.replace('.', '-')
-with open('/etc/hosts', 'a') as file:
-    file.write('127.0.0.1 %s\n' % os.getenv('HOSTNAME'))
-    file.write('127.0.0.1 %s\n' % current_host)
-os.putenv('HOSTNAME', current_host)
-
-with open('/etc/rabbitmq/rabbitmq-env.conf', 'a') as file:
-    file.write('NODENAME=rabbit@%s\n' % current_host)
 
 def get_marathon_tasks(app_id):
     http = urllib3.PoolManager()
-    response = http.request('GET', 'http://leader.mesos:8080/v2/apps%s/tasks' % app_id)
+    response = http.request('GET', '%s/v2/apps%s/tasks' % (MARATHON_URI, app_id))
     state = json.loads(response.data)
     return state['tasks']
 
-# start rabbit
-print "Starting cluster"
-endpoint = 'leader.mesos:8080'
-peers = []
-if endpoint:
-   print 'Discovering configuration from %s for app %s' % (endpoint, APP_ID)
-   tasks = get_marathon_tasks(APP_ID)
-   print 'Found %d tasks for %s' % (len(tasks), APP_ID)
-   for task in tasks:
-       if task['startedAt'] and task['host'] != host:
-           peers.append(task['host'])
 
-cluster = None
-if len(peers) > 0:
-    cluster = peers[0]
-    print 'Found cluster %s' % cluster
+def get_cluster_node_ips():
+    node_ips = []
+    if MARATHON_URI:
+        LOGGER.info('Discovering configuration from %s for app %s', MARATHON_URI, APP_ID)
+        tasks = get_marathon_tasks(APP_ID)
+        LOGGER.info('Found %d tasks for %s', len(tasks), APP_ID)
+        for task in tasks:
+            if task['startedAt'] and task['host'] != HOST_IP:
+                node_ips.append(task['host'])
+    return node_ips
 
-if not cluster:
-    subprocess.call(['service', 'rabbitmq-server', 'start'])
-else:
-    # set entry in hosts for the cluster
-    current_cluster = cluster.replace('.', '-')
-    with open('/etc/hosts', 'a') as file:
-        file.write('%s %s\n' % (cluster, current_cluster))
 
-    subprocess.call(['service', 'rabbitmq-server', 'start'])
-    time.sleep(10)
-    subprocess.call(['rabbitmqctl', 'stop_app'])
-    subprocess.call(['rabbitmqctl', 'reset'])
-    subprocess.call(['rabbitmqctl', 'join_cluster', 'rabbit@%s' % current_cluster])
-    subprocess.call(['rabbitmqctl', 'start_app'])
-    time.sleep(10)
+def get_cluster_master_node_ip(node_ips):
+    master_node_ip = None
+    if len(node_ips) > 0:
+        master_node_ip = node_ips[0]
+        LOGGER.info('Found master node %s' % master_node_ip)
+    return master_node_ip
 
-# sleep forever
-while True:
-    time.sleep(1)
+
+def configure_name_resolving(node_ips=None):
+    LOGGER.info('Adding extra entries to /etc/hosts...')
+    current_node_ip = HOST_IP
+    current_node_hostname = current_node_ip.replace('.', '-')
+    with open('/etc/hosts', 'a') as f:
+        LOGGER.info('Adding current node entries')
+        host_name_entry = '127.0.0.1 %s' % HOST_NAME
+        f.write(host_name_entry + '\n')
+        LOGGER.info('+' + host_name_entry)
+        current_host_entry = '127.0.0.1 %s' % current_node_hostname
+        f.write(current_host_entry + '\n')
+        LOGGER.info('+' + current_host_entry)
+        if node_ips:
+            LOGGER.info('Adding other node entries')
+            for node_ip in node_ips:
+                if node_ip != current_node_ip:
+                    node_hostname = node_ip.replace('.', '-')
+                    node_host_entry = '%s %s' % (node_ip, node_hostname)
+                    f.write(node_host_entry + '\n')
+                    LOGGER.info('+%s' + node_host_entry)
+    LOGGER.info('Changing hostname as %s...', current_node_hostname)
+    os.putenv('HOSTNAME', current_node_hostname)
+    return current_node_hostname
+
+
+def set_erlang_cookie():
+    cookie_file = '/var/lib/rabbitmq/.erlang.cookie'
+    rabbitmq_erlang_cookie = os.getenv('RABBITMQ_ERLANG_COOKIE', None)
+    existing_rabbitmq_erlang_cookie = None
+    cookie_file_exists = os.path.isfile(cookie_file)
+    if cookie_file_exists:
+        LOGGER.info('Found %s', cookie_file)
+        with open(cookie_file, 'r') as f:
+            existing_rabbitmq_erlang_cookie = f.read().strip()
+            LOGGER.info('Existing erlang cookie is %s', existing_rabbitmq_erlang_cookie)
+
+    if not rabbitmq_erlang_cookie and not existing_rabbitmq_erlang_cookie:
+        raise RuntimeError('No erlang cookie is set!')
+
+    if existing_rabbitmq_erlang_cookie\
+            and existing_rabbitmq_erlang_cookie != rabbitmq_erlang_cookie:
+        LOGGER.warn('%s file contents [%s] do not match RABBITMQ_ERLANG_COOKIE [%s],'
+                    ' keeping existing one.',
+                    cookie_file, existing_rabbitmq_erlang_cookie, rabbitmq_erlang_cookie)
+
+    if not existing_rabbitmq_erlang_cookie:
+        LOGGER.info('Creating erlang cookie file with secret "%s"', rabbitmq_erlang_cookie)
+        with open(cookie_file, 'w') as f:
+            f.write(rabbitmq_erlang_cookie)
+        subprocess.call(['chown', 'rabbitmq', cookie_file])
+        subprocess.call(['chmod', '600', cookie_file])
+
+
+def create_rabbitmq_config_file(node_ips=None):
+    rabbitmq_config_file = '/etc/rabbitmq/rabbitmq.config'
+    LOGGER.info('Creating %s', rabbitmq_config_file)
+    default_user = os.getenv('RABBITMQ_DEFAULT_USER', 'guest')
+    default_pass = os.getenv('RABBITMQ_DEFAULT_PASS', 'guest')
+    default_vhost = os.getenv('RABBITMQ_DEFAULT_VHOST', '/')
+    rabbitmq_management_port = os.getenv('RABBITMQ_MANAGEMENT_PORT', '/')
+    with open(rabbitmq_config_file, 'w') as f:
+        f.write('[\n')
+        f.write('  {rabbit,\n')
+        f.write('    [\n')
+        f.write('     {loopback_users, []},\n')
+        f.write('     {heartbeat, 580},\n')
+        f.write('     {default_user, <<"%s">>},\n' % default_user)
+        f.write('     {default_pass, <<"%s">>},\n' % default_pass)
+        f.write('     {default_vhost, <<"%s">>},\n' % default_vhost)
+        f.write('     {cluster_nodes, {[\n')
+        if node_ips:
+            nodes_str = ','.join(["'rabbit@%s'" % n.replace('.', '-')
+                                  for n in node_ips])
+            f.write('      %s\n' % nodes_str)
+        f.write('      ], disc}}\n')
+        f.write('    ]\n')
+        f.write('  },\n')
+        f.write('  {rabbitmq_management, [{listener, [{port, %s}]}]}\n'
+                % rabbitmq_management_port)
+        f.write('].\n')
+
+
+def configure_rabbitmq(current_node_hostname, node_ips):
+    with open('/etc/rabbitmq/rabbitmq-env.conf', 'a') as f:
+        f.write('NODENAME=rabbit@%s\n' % current_node_hostname)
+    # other settings are already in environment like port settings, see Dockerfile
+    subprocess.call(['chown', '-R', 'rabbitmq', '/var/lib/rabbitmq'])
+    set_erlang_cookie()
+    create_rabbitmq_config_file(node_ips)
+
+
+def run():
+    node_ips = get_cluster_node_ips()
+    current_node_hostname = configure_name_resolving(node_ips)
+    configure_rabbitmq(current_node_hostname, node_ips)
+    master_node_ip = get_cluster_master_node_ip(node_ips)
+    if not master_node_ip:
+        LOGGER.info("There is no other node, running as master node...")
+    else:
+        LOGGER.info("Master node %s detected, connecting to master...", master_node_ip)
+    subprocess.call(['rabbitmq-server'])
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(message)s'
+    )
+    run()
